b967253473cf4be2dc5f94a3e8dba1ef2ca0abdc
jfranc <jfranc@stanford.edu>
2022-05-20 15:05:08 -0700

    wip regex for ADGPRS

--- io_sim/toADGPRS.py
+++ io_sim/toADGPRS.py
@@ -1,16 +1,26 @@
 import logging
 import re
+import numpy as np
 import models
 from models import deadoil_blackoil
 
+# pattern_float_block = "(?:(?:(?:(?:[+-]?(?:\d*)(?:\.\d*)(?:[eE][+-]?\d+)?)\s*)+)\\n"
+pattern_float_block = "(?:(?:(?:(?:[+-]?(?:\d*)(?:\.\d*)(?:[eE][+-]?\d+)?)\s*)+)\n"
+pattern_float_block_of_block = "PVTO\n((?:(?:(?:(?:(?:[+-]?(?:\d*)(?:\.\d*)(?:[eE][+-]?\d+)?)\s*)+)\n)\/\n)+)"
+# https://regex101.com/r/SgLVyM/1
+# pattern_float_list = r"(?:(?:[+-]?(?:\d*)(?:\.\d*)(?:[eE][+-]?\d+)?)\s*)+"
+pattern_float_list = "((?:(?:[+-]?(?:\d*)(?:\.\d*)(?:[eE][+-]?\d+)?)\s*)+)(?:\n|)"
 
+#helpers
 def write_gprs_blackoil(f):
     f.write('PVTO\n \t dddd\n /\n')
     f.write('COUPLING \n FIM(FLOW{NATURAL_BLACK_OIL}) \n / \n')
 
+
 def write_gprs_deadoil(f):
     f.write('PVDO\n \t dddd\n /\n')
     f.write('COUPLING \n -- SEQ(PRESSURE{PRESSURE_NATURAL_DO}, TRANSPORT, 15) \n FIM(FLOW) \n / \n')
+
 
 def write_gprs(dict_, fname):
     with open(fname, 'w') as f:
@@ -37,6 +47,55 @@ class adgprs_model_io:
 
     def import_fluid(self):
         logging.info("ADGPRS :: import fluid")
+        lines = ''.join(open(self.iofile_).readlines())
+        fkey = re.search("\s*FLUID\n(\w+)\n/", lines)
+        print(fkey.group(1))
+
+        pvo = None
+        pvg = None
+
+        if fkey.group(1) == "BLACKOIL":
+            logging.info("Reading BLACKOIL case")
+
+            pattern = pattern_float_block_of_block
+            fpvo = re.search(pattern, lines)
+            # processing to numpy --> TODO feed it to pvt containers
+            tmp = fpvo.group(1).split('\n')[:-1]
+            key = None
+            pvo = {}
+            val = []
+            i = 0
+            for item in tmp:
+                if item == '/':
+                    continue
+                elif len(item.split()) == 1:
+                    if key:
+                        pvo[key] = np.stack(val)
+                        key = item
+                        val = []
+                    else:  # first it
+                        key = item
+                else:
+                    val.append(np.asarray(item.split(), dtype=float))
+
+        elif fkey.group(1) == "DEADOIL":
+            logging.info("Reading DEADOIL case")
+            pattern = "PVDO\n(" + pattern_float_block + ")+)/"
+            fpvo = re.search(pattern, lines)
+            # processing to numpy --> TODO feed it to pvt containers
+            pvo = np.asarray([item.split() for item in fpvo.group(1).split('\n')][:-1], dtype=float)
+
+        pattern = "PVTW\n(.*?)(?:\n|)/"
+        # pattern = "PTVW\n"+pattern_float_list+""
+        fpvw = re.search(pattern, lines)
+        pvtw = np.asarray(fpvw.group(1).split(), dtype=float)
+
+        pattern = "DENSITY\n(.*?)(?:\n|)/"
+        # pattern = "DENSITY\n("+pattern_float_list+")"
+        fdens = re.search(pattern, lines)
+        dens = np.asarray(fdens.group(1).split(), dtype=float)
+
+        print("Done")
 
     def export_fluid(self):
         logging.info("ADGPRS :: export fluid")
@@ -48,4 +107,10 @@ class adgprs_model_io:
 
         self.gprs_dict[fkey] = self.model_.fluid_.dict_
 
-        write_gprs(self.model_.fluid_.dict_)
+        write_gprs(self.model_.fluid_.dict_, self.iofile_)
+
+
+if __name__ == "__main__":
+    # mio = adgprs_model_io('../misc/gprs.deadoil.txt')
+    mio = adgprs_model_io('../misc/gprs.blackoil.txt')
+    mio.import_fluid()
